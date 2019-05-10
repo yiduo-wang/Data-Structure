37f8376e16f9d24acdc3cbdb42af523df8e8b560
yiduo-wang <1163268812@qq.com>
2019-05-10 18:42:11 +0800

    二叉树 节点

--- 二叉树/二叉树/源.cpp
+++ 二叉树/二叉树/源.cpp
@@ -40,4 +40,55 @@
 	  return;
   }
 
+
+
+  /**
+	 * 动态创建二叉查找树
+	 *
+	 * @param treeRoot 根节点
+	 * @param value    节点的值
+	 */
+  public static void createTree(TreeRoot treeRoot, int value) {
+
+	  //如果树根为空(第一次访问)，将第一个值作为根节点
+	  if (treeRoot.getTreeRoot() == null) {
+		  TreeNode treeNode = new TreeNode(value);
+		  treeRoot.setTreeRoot(treeNode);
+
+	  }
+	  else {
+
+		  //当前树根
+		  TreeNode tempRoot = treeRoot.getTreeRoot();
+
+		  while (tempRoot != null) {
+			  //当前值大于根值，往右边走
+			  if (value > tempRoot.getValue()) {
+
+				  //右边没有树根，那就直接插入
+				  if (tempRoot.getRightNode() == null) {
+					  tempRoot.setRightNode(new TreeNode(value));
+					  return;
+				  }
+				  else {
+					  //如果右边有树根，到右边的树根去
+					  tempRoot = tempRoot.getRightNode();
+				  }
+			  }
+			  else {
+				  //左没有树根，那就直接插入
+				  if (tempRoot.getLefTreeNode() == null) {
+					  tempRoot.setLefTreeNode(new TreeNode(value));
+
+					  return;
+				  }
+				  else {
+					  //如果左有树根，到左边的树根去
+					  tempRoot = tempRoot.getLefTreeNode();
+				  }
+			  }
+		  }
+	  }
+  }
+
  
